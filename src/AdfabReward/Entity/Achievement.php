f7c03426879db1ff88536cf0469d16b4f03c3a0b
GrG <gregory.besson@adfab.fr>
2013-06-01 14:46:58 +0200

    Les entités utilisent désormais UserInterface afin de permettre l'extension de l'entity AdfabUser/Entity/User

--- src/AdfabReward/Entity/Achievement.php
+++ src/AdfabReward/Entity/Achievement.php
@@ -21,7 +21,7 @@ class Achievement
     protected $id;
 
     /**
-     * @ORM\ManyToOne(targetEntity="AdfabUser\Entity\User")
+     * @ORM\ManyToOne(targetEntity="AdfabUser\Entity\UserInterface")
      * @ORM\JoinColumn(name="user_id", referencedColumnName="user_id")
      **/
     protected $user;
