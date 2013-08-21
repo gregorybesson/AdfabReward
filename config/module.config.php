1fdab4e0e65c3f39b899618e764311930295fcf7
GrG <gregory.besson@adfab.fr>
2013-08-20 18:44:03 -0700

    PLAYGROUND-42

--- config/module.config.php
+++ config/module.config.php
@@ -23,7 +23,8 @@ return array(
 
     'view_manager' => array(
         'template_path_stack' => array(
-            'adfabreward' => __DIR__ . '/../view',
+            __DIR__ . '/../view/admin',
+        	__DIR__ . '/../view/frontend',
         ),
     ),
 
